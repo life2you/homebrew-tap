a3b8cfd019205787678a360c27ec3c9ff4caea2d
life2you <henry@henglin.com>
2026-05-12 18:10:37 +0800

    Update gmux to 0.1.7

--- Formula/gmux.rb
+++ Formula/gmux.rb
@@ -1,8 +1,8 @@
 class Gmux < Formula
   desc "Terminal Git workflow tool for multi-env branch sync and GitLab MR automation"
   homepage "https://github.com/life2you/gmux"
-  url "https://github.com/life2you/gmux/archive/refs/tags/v0.1.6.tar.gz"
-  sha256 "4d11395dd91750515c4e9042f5d5bd02b82076e4439f2caf42612e197e04a69e"
+  url "https://github.com/life2you/gmux/archive/refs/tags/v0.1.7.tar.gz"
+  sha256 "3b7ca313a5c89f62d825b2f003bc54bb2ae6e9b764a55454285eeb2698d10dd4"
   license "MIT"
 
   depends_on "rust" => :build
