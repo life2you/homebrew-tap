5b67390568266d699a5acf04d7badb31ef16f718
life2you <henry@henglin.com>
2026-05-13 20:43:20 +0800

    Update gwtm to 0.1.7

--- Formula/gwtm.rb
+++ Formula/gwtm.rb
@@ -1,8 +1,8 @@
 class Gwtm < Formula
   desc "Git worktree manager for local multi-project workflows"
   homepage "https://github.com/life2you/gwtm"
-  url "https://github.com/life2you/gwtm/archive/refs/tags/v0.1.6.tar.gz"
-  sha256 "cbed880710d7eff86e1df57218e96b6d33fb21a215d18f288b91bf639ef98978"
+  url "https://github.com/life2you/gwtm/archive/refs/tags/v0.1.7.tar.gz"
+  sha256 "789f96f3790b285bb239bf5d71dc29daf23d0ff3f2e46fe76a8fa31d9e3dd63e"
   license "MIT"
 
   depends_on "rust" => :build
