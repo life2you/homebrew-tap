b454cdcb9972e78c784053a9c03a7e27c7c6932b
life2you <henry@henglin.com>
2026-05-13 20:46:00 +0800

    Set explicit gwtm version to 0.1.7

--- Formula/gwtm.rb
+++ Formula/gwtm.rb
@@ -1,6 +1,7 @@
 class Gwtm < Formula
   desc "Git worktree manager for local multi-project workflows"
   homepage "https://github.com/life2you/gwtm"
+  version "0.1.7"
   url "https://github.com/life2you/gwtm/archive/refs/tags/v0.1.7.tar.gz"
   sha256 "789f96f3790b285bb239bf5d71dc29daf23d0ff3f2e46fe76a8fa31d9e3dd63e"
   license "MIT"
