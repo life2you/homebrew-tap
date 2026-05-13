693ebd765ed69f9f6065adb84b89fd53efcef2ce
life2you <henry@henglin.com>
2026-05-13 11:56:46 +0800

    feat: update wetwin formula to v0.1.1

--- Formula/wetwin.rb
+++ Formula/wetwin.rb
@@ -1,14 +1,23 @@
 class Wetwin < Formula
   desc "Lightweight macOS WeChat multi-instance manager with a terminal UI"
   homepage "https://github.com/life2you/wetwin"
-  url "https://github.com/life2you/wetwin/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "9f30d8e641450ab0e08213ae75bf27530cad5e152d648c9822a43ccec533f21b"
+  version "0.1.1"
   license "MIT"
 
-  depends_on "rust" => :build
+  on_macos do
+    on_arm do
+      url "https://github.com/life2you/wetwin/releases/download/v0.1.1/wetwin-aarch64-apple-darwin.tar.gz"
+      sha256 "35a4eb9239a1dfad941c8e4ccf344e406870273a52be30427430846ee91245d7"
+    end
+
+    on_intel do
+      url "https://github.com/life2you/wetwin/releases/download/v0.1.1/wetwin-x86_64-apple-darwin.tar.gz"
+      sha256 "02a31157017a08aa7a4dd305e8395cf7a301074ae711dfbd6897da50125c6af8"
+    end
+  end
 
   def install
-    system "cargo", "install", *std_cargo_args(path: ".")
+    bin.install "wetwin"
   end
 
   test do
