74cbe9302e4132303a0696c0f8130f52a07497c7
life2you <henry@henglin.com>
2026-05-18 16:32:20 +0800

    Update gmux to 0.1.9

--- Formula/gmux.rb
+++ Formula/gmux.rb
@@ -1,18 +1,18 @@
 class Gmux < Formula
   desc "Terminal Git workflow tool for multi-env branch sync and GitLab MR automation"
   homepage "https://github.com/life2you/gmux"
-  version "0.1.8"
+  version "0.1.9"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/life2you/gmux/releases/download/v0.1.8/gmux-aarch64-apple-darwin.tar.gz"
-      sha256 "030573c7e16c80c3ceed16e249003993e7a9ad6cfa8d01cf1ae4ca8eb7e287d1"
+      url "https://github.com/life2you/gmux/releases/download/v0.1.9/gmux-aarch64-apple-darwin.tar.gz"
+      sha256 "1c451de3a5179b67c4ef648dbb46ae6a613c99f321f9b49820e8e83bcb63fc87"
     end
 
     on_intel do
-      url "https://github.com/life2you/gmux/releases/download/v0.1.8/gmux-x86_64-apple-darwin.tar.gz"
-      sha256 "fac47b520cad2e217035826551ef4274c9da52002c14f71416a98d313b46b4fb"
+      url "https://github.com/life2you/gmux/releases/download/v0.1.9/gmux-x86_64-apple-darwin.tar.gz"
+      sha256 "e27f1bcb917a164fbe7291d298749f07b9b8db81edd03135c50eef55e67ae116"
     end
   end
 
