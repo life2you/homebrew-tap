f8ea7ac61ab845f9131acbbfbe915eb1d8ae3946
life2you <henry@henglin.com>
2026-06-12 11:45:25 +0800

    Add gworkbench 0.1.1

--- Casks/gworkbench.rb
+++ Casks/gworkbench.rb
@@ -1,14 +1,14 @@
 cask "gworkbench" do
-  version "0.1.0"
+  version "0.1.1"
 
   on_arm do
-    sha256 "a98b64bd9e9e7d44a4e32e59817e3d230d32d06dd9182f73cabc680bb94342ec"
-    url "https://github.com/life2you/gworkbench/releases/download/v0.1.0/GWorkbench-macos-arm64-v0.1.0.zip"
+    sha256 "444c78daf134353afb3c40cf53d9e01ca48aef97c96a48f5af06869912fc4ee6"
+    url "https://github.com/life2you/gworkbench/releases/download/v0.1.1/GWorkbench-macos-arm64-v0.1.1.zip"
   end
 
   on_intel do
-    sha256 "0ed86b8ba426051ba72cbce9db72ff9ac03b99829ec212e4cc318f41f2549e3a"
-    url "https://github.com/life2you/gworkbench/releases/download/v0.1.0/GWorkbench-macos-x86_64-v0.1.0.zip"
+    sha256 "9aebf028187756978d8064fd24fdefdc9d04db06378acf35f37b18624306bbd7"
+    url "https://github.com/life2you/gworkbench/releases/download/v0.1.1/GWorkbench-macos-x86_64-v0.1.1.zip"
   end
 
   name "GWorkbench"
