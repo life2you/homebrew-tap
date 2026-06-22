ffc24029995762f2712c60f5f6090762e7e2dcc3
life2you <henry@henglin.com>
2026-06-22 09:43:09 +0800

    Update codexsmtui formula to 0.1.3

--- Formula/codexsmtui.rb
+++ Formula/codexsmtui.rb
@@ -1,18 +1,18 @@
 class Codexsmtui < Formula
   desc "Terminal-first TUI for managing local OpenAI Codex CLI sessions"
   homepage "https://github.com/life2you/codexsmTui"
-  version "0.1.2"
+  version "0.1.3"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/life2you/codexsmTui/releases/download/v0.1.2/codexsmtui-aarch64-apple-darwin.tar.gz"
-      sha256 "7c9ca9131e116aaff292011ae27dbeb154edffece28aa67898ad0f4b2a24018d"
+      url "https://github.com/life2you/codexsmTui/releases/download/v0.1.3/codexsmtui-aarch64-apple-darwin.tar.gz"
+      sha256 "9138234cf57ce53a71915f6c15486e792171b5b886580b7f95b7e2174895da6b"
     end
 
     on_intel do
-      url "https://github.com/life2you/codexsmTui/releases/download/v0.1.2/codexsmtui-x86_64-apple-darwin.tar.gz"
-      sha256 "b03171560cbe5fca4638a525beeb2696076171fc7a6d65154956b919a1bf2900"
+      url "https://github.com/life2you/codexsmTui/releases/download/v0.1.3/codexsmtui-x86_64-apple-darwin.tar.gz"
+      sha256 "af778e9c0c8aea76c42c05d236c4f265fe24d70bc548abfa791dbc20cc7fce8b"
     end
   end
 
